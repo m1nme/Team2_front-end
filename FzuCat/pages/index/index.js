c298864f9dd5262a2a64df64a0e94407846759f7
Yamyyy <2396649622@qq.com>
2020-11-21 08:28:48 +0800

    [ADD]增加档案

--- FzuCat/pages/index/index.js
+++ FzuCat/pages/index/index.js
@@ -4,46 +4,53 @@ const app = getApp()
 
 Page({
   data: {
-    catList:[{"id":"123","picUrl":"1234","records":["11月6日14：30 主食x1","11月6日19：30 鸡肉x1"]}],
-    skeletonLoding:true,
+    skeletonLoading:true,
+    catList:null
   },
 
   //事件处理函数
+  changeTabs:function(e){
+    this.getCatListByAddress(e.detail.activeKey);
+   
+  },
 
+  getCatListByAddress:function(address){
+
+    var that = this;
+    var token = wx.getStorageSync('token') //获取stroage的token
+   wx.request({
+     method:"POST",
+     dataType:"json",
+     url: 'https://iminx.cn/api/wxapp/showCatsList/', //仅为示例，并非真实的接口地址
+     data: {
+       token: token, //带上token
+       address:address
+     },
+     header: {
+       'content-type': 'application/json' // 默认值
+     },
+     success(res) {
+      that.setData({skeletonLoading:false})
+       console.log("getCatList"+res.data.msg)
+       
+       that.setData({catList:res.data.data})
+      //  that.data.catList=res.data.data
+       console.log(that.data.catList)
+     },
+     fail(){
+       console.log("失败")
+     }
+   })
+ },
   onLoad: function () {
-
     //授权登录
     wx.getUserInfo()
-    
+    app.checkLoginReadyCallback = res => {
+      this.getCatListByAddress("一区")
+    }
   },
-  tap:function(){
-    this.data.catList.push({"id":"123","picUrl":"1234","records":["11月6日14：30 主食x1","11月6日19：30 鸡肉x1"]})
-    this.data.catList.push({"id":"124","picUrl":"1244","records":["11月6日14：30 主食x1","11月6日19：30 鸡肉x1"]})
-    console.log(this.data.catList.length)
-  },
-
-  
-  getCatListByAddress:function(){
-     var token = wx.getStorageSync('token') //获取stroage的token
-    console.log(token);
-    wx.request({
-      method:"POST",
-      dataType:"json",
-      url: 'https://iminx.cn/api/wxapp/showCatsList/', //仅为示例，并非真实的接口地址
-      data: {
-        token: token, //带上token
-        address:"ALL"
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success(res) {
-        console.log(res.data.msg)
-      },
-      fail(){
-        console.log("失败")
-      }
-    })
-  }
+  onReady:function(){
+    this.changeTabs
+}
 
 })
