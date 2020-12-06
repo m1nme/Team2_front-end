97d70371c14a2fb380787165b9e9659a6c4b9395
Jamie-Jing <70847889+Jamie-Jing@users.noreply.github.com>
2020-12-06 20:08:16 +0800

    [ADD]修改帖子详情

--- FzuCat/pages/mine/minepost/post/post.js
+++ FzuCat/pages/mine/minepost/post/post.js
@@ -1,66 +1,80 @@
 // pages/post/post.js
+var util = require('../../../../utils/util')
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    postInfo:null,
+    title: "有谁知道它最近在哪吗？",
+    content: "之前都在35栋楼下车库看到它，不知道现在在哪了,有谁知道它最近在哪吗？",
+    urlList: ['https://iminx-1258939911.cos.ap-chengdu.myqcloud.com/fzucats/20201113230601.jpg', 'https://iminx-1258939911.cos.ap-chengdu.myqcloud.com/fzucats/20201113230601.jpg', 'https://iminx-1258939911.cos.ap-chengdu.myqcloud.com/fzucats/20201113230601.jpg', 'https://iminx-1258939911.cos.ap-chengdu.myqcloud.com/fzucats/20201113230601.jpg'],
+    userName: "故渊",
+    userUrl: "https://thirdwx.qlogo.cn/mmopen/vi_32/0dAfHYgu3XIj2ACwX9IyR4S2rs6hTjPJzeGuNLZQQuRR7wILElvxv8et6VCeE9fpl7GbwdKWSyZic2bEfoRlvow/132",
+    catId: 1,
+    time: "2020-11-22T14:48:56.316Z",
+    postId: 2
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  getPostInfoByuserId: function (postId) {
+    var token = wx.getStorageSync('token')
+    var that = this;
+    wx.request({      
+      url: 'https://iminx.cn/api/wxapp/getPostInfo/',
+      method:'post',
+      dataType:"json",
+      data:{
+        token: token,
+        postId: postId,
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res){
+        console.log("postInfo "+res.data.msg)
+        console.log(token)
+        console.log(res.data.data)
+        that.setData({postInfo:res.data.data})
+      },
+      fail(){
+        console.log("网络请求失败")
+      }
+    })
+  },
+  
+  onload:function(){
+    //wx.getfeedInfo()
+    app.getfeedInfo = res =>{
+        this.getfeedInfoByuserId("USER");
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    this.data.time.forEach((item) => {
+      item = until.formatTime(item)
+      item.flag = false
+    })
+    this.setData({
+      time: this.data.time
+    })
+  },
+  onShow: function () {
+    this.data.minePost_list.forEach((item) => {
+      item.time = util.formatTime(item.time)
+      item.flag = false
+    })
+    this.setData({
+      minePost_list: this.data.minePost_list
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  PreviewImg: function (e) {
+    let index = e.target.dataset.index;
+    let that = this;
+    //console.log(that.data.tempFilePaths[index]);
+    //console.log(that.data.tempFilePaths);
+    wx.previewImage({
+      current: that.data.urlList[index],
+      urls: that.data.urlList,
+    })
   }
 })
